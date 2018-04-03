d51fae2ab4bbdbbdbdc77b82ed84b79ddd129863
DakotaFrico33 <marco.cjpascolo@gmail.com>
2018-04-03 02:02:30 -0600

    inserted USART functions to type to LCD from keyboard

--- main.c
+++ main.c
@@ -3,28 +3,78 @@
 #include "myUSART.h"
 #include "myLCD.h"
 
+void outputString(char *str)
+{
+	int i = 0;
+	int j = 0;
+	while (str[i]!='\0')
+	{
+		LCD_write_char(str[i]);
+		i++;
+		j++;
+		if (j==16) //end of line, wrap to second line
+		{
+			LCD_write_instruction(LCD_4bit_cursorSET|LineTwoStart);
+			_delay_us(80);
+		}
+
+		if (j==32) //end of line, overwrite first line
+		{
+			LCD_write_instruction(LCD_4bit_cursorSET|LineOneStart);
+			_delay_ms(1000);
+			j=0;
+		}
+		
+
+	}
+}
+
+void typeMsg()
+{
+	char key;
+
+	key = USARTreceive(); //type on keyboard
+	USARTtransmit(key);//echo bit back to terminal
+	_delay_us(80);
+	LCD_write_char(key); // write char to LCD screen
+	_delay_us(80);
+}
+
+
 
 int main()
 {
 		DDRB = 0b00100011;
 		DDRD = 0xF0;
-		char MyChar = 'A';
+		char myStr[100] = "Hello!";
+		char MyChar = 'C';
 		// Initailize the LCD for 4-bit mode, two lines, and 5 x 8 characters //
 		// Inits found on Page 46 of datasheet and Table 6 for function set instructions //
 		LCD_init();
+		initUSART();
 
-		// Write a single character //
-		LCD_write_char(MyChar);
+		// Write a single character or a string //
+		//LCD_write_char(MyChar);
+		outputString(myStr);
 
 		// line two //
-		LCD_write_instruction(LCD_4bit_cursorSET|LineTwoStart);
-		_delay_us(80); // delay must be > 37us - datasheet forgets to mention this //
-		LCD_write_char(MyChar);
+		//LCD_write_instruction(LCD_4bit_cursorSET|LineTwoStart);
+		//_delay_us(80); // delay must be > 37us - datasheet forgets to mention this //
+		//LCD_write_char(MyChar);
+
+		_delay_ms(1000);
+
+		LCD_write_instruction(LCD_4bit_displayCLEAR);
+		_delay_us(80);
+		LCD_write_instruction(LCD_4bit_cursorSET|LineOneStart);
+		_delay_us(80);
+
+
+
 
 		while(1)
 		{
-			PORTB ^= 0x20;
-			_delay_ms(500);
+			typeMsg();
 		}
 
 return 1;
